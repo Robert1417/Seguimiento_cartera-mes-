686b2e4538ea1f686015a38ca99afa6e6f1b37ac
Robert1417 <roberto.chapman@gobravo.com.co>
2026-08-11 18:52:04 -0500

    Update sync_bucket.py

--- sync_bucket.py
+++ sync_bucket.py
@@ -176,7 +176,7 @@ def main():
         print("No hay actividad reciente de hoy, ayer o antier")
         return
 
-    df_recent["_peso"] = 5
+    df_recent["_peso"] = 20
 
     quotas = (
         df_recent
